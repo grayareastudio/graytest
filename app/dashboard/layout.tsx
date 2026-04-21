9728f68200966e01bafecfc249c6a23af58472ce
Ariq Fadhil Musyaffa <ariqfms@gmail.com>
2026-04-21 14:42:50 +0700

    fix: fix layout dashboard and add authentication

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,40 +1,24 @@
-import type { Metadata } from "next";
-import { Fira_Sans, Hedvig_Letters_Serif } from "next/font/google";
-import "../globals.css";
-import { Header } from "@/components/layout/Header";
+// app/dashboard/layout.tsx
 import { Footer } from "@/components/layout/Footer";
+import { Header } from "@/components/layout/Header";
+import { getCurrentUser } from "@/lib/actions/auth-actions";
+import { redirect } from "next/navigation";
 
-const firaSans = Fira_Sans({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  variable: "--font-sans",
-});
-
-const hedvig = Hedvig_Letters_Serif({
-  subsets: ["latin"],
-  variable: "--font-serif",
-});
-
-export const metadata: Metadata = {
-  title: "Gray Area",
-  description: "gray area",
-};
-
-export default function RootLayout({
+export default async function DashboardLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const user = await getCurrentUser();
+  if (!user) {
+    redirect("/login?next=/dashboard");
+  }
+
   return (
-    <html
-      lang="en"
-      className={`${firaSans.variable} ${hedvig.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col bg-linear-to-tr from-black to-[#171717]">
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
   );
 }
