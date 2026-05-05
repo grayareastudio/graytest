f89c212a11032b360817acf3728d3a73df1decd3
Ariq Fadhil Musyaffa <ariqfms@gmail.com>
2026-05-05 10:21:59 +0700

    style: add close button in pre-test page / pop ups

--- app/test/[type]/intro/page.tsx
+++ app/test/[type]/intro/page.tsx
@@ -6,6 +6,8 @@ import { INTRO_SLIDES_DATA } from "@/lib/data/intro-slides";
 import { SlideRenderer } from "@/components/intro/SlideRenderer";
 import { Button } from "@/components/ui/Button";
 import GradientText from "@/components/ui/GradientText";
+import Image from "next/image";
+import deleteIcon from "@/assets/icons/delete.svg";
 
 export default function TestIntroPage() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -36,8 +38,19 @@ export default function TestIntroPage() {
     }
   };
 
+  const handleClose = () => {
+    router.push(`/test/${testType}/questions`);
+  }
+
   return (
     <div className="flex-1 flex flex-col items-center justify-center px-6 md:px-12 lg:px-39 py-10 md:py-20 w-full">
+      <button
+      onClick={handleClose}
+        className="text-white/70 hover:text-white transition-colors hover:cursor-pointer absolute top-10 right-10"
+      >
+        <Image src={deleteIcon} alt="close" width={36} height={36} />
+      </button>
+
       <h1 className="font-serif text-4xl md:text-5xl mb-8 text-center max-w-4xl">
         <GradientText>{current.title}</GradientText>
       </h1>
